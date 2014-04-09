56c222a8cafe97808ffab1cb65cf2587fd524baa
Matthew Green <matthew@newedgeengineering.com>
2014-04-09 14:08:57 +0100

    Added elements to pom.xml to match example plugins

--- src/main/java/com/newedgeengineering/flow/plugin/rundeck/PluginService.java
+++ src/main/java/com/newedgeengineering/flow/plugin/rundeck/PluginService.java
@@ -1,5 +1,8 @@
 package com.newedgeengineering.flow.plugin.rundeck;
 
+import net.xeoh.plugins.base.Plugin;
+import org.activiti.engine.delegate.DelegateExecution;
+
 public interface PluginService extends Plugin {
     void addGrailsAppContext(Object context);
     void triggerStep(DelegateExecution execution);
